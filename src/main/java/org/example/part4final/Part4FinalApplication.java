24fc9f7248ac8976a3c23194edea88188a997dd3
Juce8888 <keleallison6@gmail.com>
2024-02-17 22:17:09 -0600

    First commit

---
+++ src/main/java/org/example/part4final/Part4FinalApplication.java
@@ -0,0 +1,13 @@
+package org.example.part4final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part4FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Part4FinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/part4final/Part4FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.part4final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Part4FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
